098f77c9f8125c12a66023dc0f4967772980f26c
Rudraksh TIku <72981271+RudyDaBot@users.noreply.github.com>
2021-11-07 11:29:38 +0530

    red is sus

---
+++ among.js
@@ -0,0 +1,19 @@
+const susify = (arg) => {
+    return(arg + " is sus");
+}
+
+const eject = (name, noOfImposters) => {
+
+    if (noOfImposters !== 1 && noOfImposters !== 0) {
+        let imposterAmount = noOfImposters - 1;
+        return(`${name} is an impostor. ${imposterAmount} imposters remain`)
+    }
+
+    else 
+    {
+        console.error("Invalid Value. Please set value higher than 2")
+    }
+
+   
+}
+
